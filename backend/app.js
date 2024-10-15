e93c87dc2dc1e009b42c35f3e552d4e549e5f0bc
saikrishnabanda1 <157004015+saikrishnabanda1@users.noreply.github.com>
2024-10-15 20:55:32 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,7 @@ const corsConfig = {
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
-app.use(cors(corsConfig))
+app.use(cors())
 app.options("", cors(corsConfig))
 
 //routes
